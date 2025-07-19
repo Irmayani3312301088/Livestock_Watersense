46abae682823379aa033f4e6b8940e69afa3130d
irma <yirma9867@gmail.com>
2025-07-19 18:50:04 +0700

    hapus .env

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,5 +1,7 @@
 const { Sequelize, DataTypes } = require('sequelize');
-require('dotenv').config();
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'production' ? '.env' : '.env.local',
+});
 
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'livestock_db',
--- backend/testSend.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const sendEmail = require('./utils/sendEmail');
-
-sendEmail('alamatemailmu@gmail.com', 'Test Kirim OTP', 'Ini email tes OTP')
-  .then(() => console.log('Email berhasil dikirim!'))
-  .catch(err => console.error('Gagal kirim email:', err));
